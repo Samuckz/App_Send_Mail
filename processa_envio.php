6c46f892971ecffc254bec23db3e6a8c9cb64001
Samuel Viana <contato.santanasamuel@gmail.com>
2023-03-01 09:02:13 -0300

    Add files via upload

---
+++ processa_envio.php
@@ -0,0 +1,13 @@
+<?php  
+
+	
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/Exception.php";
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/OAuthTokenProvider.php";	
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/OAuth.php";
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/PHPMailer.php";
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/POP3.php";
+	require "../../../app_send_email/bibliotecas/PHPMailer/src/SMTP.php";
+
+	require "../../../app_send_email/processa_envio.php"
+
+?>
